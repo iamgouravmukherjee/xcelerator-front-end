1d411389149a319c80deaf340ccf17486067b27e
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-04-15 20:22:06 +0530

    added actions

--- src/components/DetailedView.js
+++ src/components/DetailedView.js
@@ -25,6 +25,39 @@ class DetailedView extends Component {
             })
       }
    }
+
+   handleActions = (actionType, id) => {
+      axios.patch(`http://localhost:3006/api/posts/${id}`, { type: actionType })
+         .then(response => {
+            // console.log('response from server', response);
+            let posts = [...this.state.post];
+            // posts = posts.map(post => {
+            switch (actionType) {
+               case "liked":
+                  posts[0]['liked'] = !posts[0]['liked'];
+                  posts[0]['disliked'] = false;
+                  break;
+               case "disliked":
+                  posts[0]['disliked'] = !posts[0]['disliked'];
+                  posts[0]['liked'] = false;
+                  break;
+               case "bookmarked":
+                  posts[0]['bookmarked'] = !posts[0]['bookmarked'];
+                  break;
+               default:
+                  break;
+            }
+            // })
+            this.setState((previousState) => {
+               return { ...previousState, post:  posts };
+            })
+         })
+         .catch(error => {
+            console.log('error occuring while liking', error);
+         })
+   }
+
+
    render() {
       let post = null;
       if (this.state.loading) {
@@ -32,14 +65,29 @@ class DetailedView extends Component {
       } else {
          post = (
             <div className="item item--full detailed">
-               <div className={`postImage image${Math.floor(Math.random() * (6 - 1)) + 1}`}></div>
+               <div style={{ backgroundImage: `url(${JSON.stringify(this.state.post[0]['imageUrl'])})` }} className={`postImage`}></div>
                <div className="item__details">
                   <div className="postTitle"> {this.state.post[0]['title']}</div>
-                  <div> 
+                  <div>
                      <p>{this.state.post[0]['body']}</p>
                      <p>{this.state.post[0]['body']}</p>
                   </div>
+                  <div className="actions">
+                     <i
+                        className={this.state.post[0]['liked'] ? 'fas fa-thumbs-up' : 'far fa-thumbs-up'}
+                        onClick={() => this.handleActions('liked', this.state.post[0]['id'])} >
+                     </i>
+                     <i
+                        className={this.state.post[0]['disliked'] ? 'fas fa-thumbs-down' : 'far fa-thumbs-down'}
+                        onClick={() => this.handleActions('disliked', this.state.post[0]['id'])} >
+                     </i>
+                     <i
+                        className={this.state.post[0]['bookmarked'] ? 'fas fa-bookmark' : 'far fa-bookmark'}
+                        onClick={() => this.handleActions('bookmarked', this.state.post[0]['id'])} >
+                     </i>
+                  </div>
                </div>
+
             </div>
          );
       }
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -30,35 +30,42 @@ class ListView extends Component {
          })
    }
 
+
    handleActions = (actionType, id) => {
-      console.log('actionType', actionType);
-      switch (actionType) {
-         case 'liked':
+      axios.patch(`http://localhost:3006/api/posts/${id}`, { type: actionType })
+         .then(response => {
+            // console.log('response from server', response);
             let posts = [...this.state.posts];
             posts = posts.map(post => {
-               if(post.id === id) {
-                  post.liked = !post.liked;
-                  console.log('post', post);
+               switch (actionType) {
+                  case "liked":
+                     if (post.id === id) {
+                        post.liked = !post.liked;
+                        post.disliked = false;
+                     }
+                     return post;
+                  case "disliked":
+                     if (post.id === id) {
+                        post.disliked = !post.disliked;
+                        post.liked = false;
+                     }
+                     return post;
+                  case "bookmarked":
+                     if (post.id === id) {
+                        post.bookmarked = !post.bookmarked;
+                     }
+                     return post;
+                  default:
+                     return post;
                }
-               return post;
             })
             this.setState((previousState) => {
-               return { ...previousState, posts};
+               return { ...previousState, posts };
             })
-            break;
-         case 'disliked':
-            this.setState((previousState) => {
-               return { 'disliked': !previousState['disliked'], 'liked': false };
-            })
-            break;
-         case 'bookmarked':
-            this.setState((previousState) => {
-               return { 'bookmarked': !previousState['bookmarked'] };
-            })
-            break;
-         default: 
-            break;
-      }
+         })
+         .catch(error => {
+            console.log('error occuring while liking', error);
+         })
    }
 
    render() {
@@ -69,7 +76,7 @@ class ListView extends Component {
          posts = this.state.posts.map((post, index) => {
             return (
                <div key={post.id} className={index % 2 === 0 ? 'item item--medium' : ' item item--large'}>
-                  <div className={`postImage image${Math.floor(Math.random() * (6 - 1)) + 1}`}></div>
+                  <div style={{ backgroundImage: `url(${JSON.stringify(post.imageUrl)})` }} className={`postImage`}></div>
                   <Link to={{
                      pathname: `/posts/${post.id}`,
                      state: {
@@ -84,13 +91,13 @@ class ListView extends Component {
                   <div className="actions">
                      <i
                         className={post.liked ? 'fas fa-thumbs-up' : 'far fa-thumbs-up'}
-                        onClick={() => this.handleActions('liked',post.id)} ></i>
+                        onClick={() => this.handleActions('liked', post.id)} ></i>
                      <i
                         className={post.disliked ? 'fas fa-thumbs-down' : 'far fa-thumbs-down'}
-                        onClick={() => this.handleActions('disliked',post.id)} ></i>
+                        onClick={() => this.handleActions('disliked', post.id)} ></i>
                      <i
                         className={post.bookmarked ? 'fas fa-bookmark' : 'far fa-bookmark'}
-                        onClick={() => this.handleActions('bookmarked',post.id)}></i>
+                        onClick={() => this.handleActions('bookmarked', post.id)}></i>
                   </div>
                </div>
             );
